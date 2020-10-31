76d9e64f9c041737493191e43f8a763e7c074cbe
jiangcaixi1 <jiangcaixi1@xiaomi.com>
2020-10-31 22:39:17 +0800

    10/31/2020 Two Sum Add Two Numbers

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,44 @@
+/**
+ * 10/31/2020
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode((l1.val + l2.val) % 10);
+        ListNode node = head;
+        int status = head.val / 10;
+        int num;
+        l1 = l1.next;
+        l2 = l2.next;
+        while (l1 != null || l2 != null || status != 0) {
+            num = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + status;
+            node.next = new ListNode(num % 10);
+            node = node.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            status = num / 10;
+        }
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ src/TwoSum.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 10/31/2020
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer index = numMap.get(target - nums[i]);
+            if (index != null) {
+                return new int[]{i, index};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
