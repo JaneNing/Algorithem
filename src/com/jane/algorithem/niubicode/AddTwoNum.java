0cfd5307227150fb4adfa9c5203a927db052e876
gwy <guwy16@163.com>
2020-11-27 09:53:58 +0800

    1127

---
+++ src/com/jane/algorithem/niubicode/AddTwoNum.java
@@ -0,0 +1,93 @@
+package com.jane.algorithem.niubicode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class AddTwoNum {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode rex = new ListNode(0);
+        ListNode head = rex;
+        int key = 0;
+        int sum = 0;
+        ListNode n1 = l1;
+        ListNode n2 = l2;
+        while (n1 != null && n2 != null) {
+            int val;
+            sum = n1.val + n2.val + key;
+            if (sum >= 10) {
+                key = 1;
+                val = sum - 10;
+            } else {
+                key = 0;
+                val = sum;
+            }
+            rex.next = new ListNode(val);
+            rex = rex.next;
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        if (n1 == null) {
+            if (n2 != null) {
+                if (key == 1) {
+                    do {
+                        int val;
+                        sum = n2.val + key;
+                        if (sum >= 10) {
+                            key = 1;
+                            val = sum - 10;
+                        } else {
+                            key = 0;
+                            val = sum;
+                        }
+                        rex.next = new ListNode(val);
+                        rex = rex.next;
+                        n2=n2.next;
+                    } while (n2 != null);
+                    if (key == 1) {
+                        rex.next = new ListNode(1);
+                    }
+                } else {
+                    rex.next = n2;
+                }
+            } else {
+                if (key == 1) {
+                    rex.next=new ListNode(1);
+                }
+            }
+        } else {
+            if (key == 1) {
+                do {
+                    int val;
+                    sum = n1.val + key;
+                    if (sum >= 10) {
+                        key = 1;
+                        val = sum - 10;
+                    } else {
+                        key = 0;
+                        val = sum;
+                    }
+                    rex.next = new ListNode(val);
+                    rex = rex.next;
+                    n1=n1.next;
+                } while (n1 != null);
+                if (key == 1) {
+                    rex.next = new ListNode(1);
+                }
+            } else {
+                rex.next = n1;
+            }
+        }
+        return head.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ src/com/jane/algorithem/niubicode/SortString.java
@@ -0,0 +1,30 @@
+package com.jane.algorithem.niubicode;
+
+
+public class SortString {
+
+
+    /**
+     * 给你一个字符串 s ，请你根据下面的算法重新构造字符串：
+     *
+     * 从 s 中选出 最小 的字符，将它 接在 结果字符串的后面。
+     * 从 s 剩余字符中选出 最小 的字符，且该字符比上一个添加的字符大，将它 接在 结果字符串后面。
+     * 重复步骤 2 ，直到你没法从 s 中选择字符。
+     * 从 s 中选出 最大 的字符，将它 接在 结果字符串的后面。
+     * 从 s 剩余字符中选出 最大 的字符，且该字符比上一个添加的字符小，将它 接在 结果字符串后面。
+     * 重复步骤 5 ，直到你没法从 s 中选择字符。
+     * 重复步骤 1 到 6 ，直到 s 中所有字符都已经被选过。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/increasing-decreasing-string
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @return
+     */
+    private String sortString_1370(String s) {
+        char[] chars = s.toCharArray();
+        char[] result= new char[chars.length];
+
+
+        return "";
+    }
+}
