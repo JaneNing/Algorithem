b4c16fc7ede07bb906a1868f4f7dc1fc91157628
jiangcaixi1 <jiangcaixi1@xiaomi.com>
2020-12-13 17:03:58 +0800

    12/13/2020

---
+++ src/com/jane/algorithem/leetcode/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+package com.jane.algorithem.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
