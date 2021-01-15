ff3f1c6f34ae210d0b5bb785131d71b2c537284d
hanshan <hanshan@xiaomi.com>
2021-01-15 17:09:04 +0800

    x5sh1-2012-01-15

---
+++ src/com/x5sh1/leetcode-cn/MostStonesRemovedWithSameRowOrColumn.java
@@ -0,0 +1,43 @@
+package com.x5sh1.leetcode-cn
+
+/*
+https://leetcode-cn.com/problems/most-stones-removed-with-same-row-or-column/
+ */
+class MostStonesRemovedWithSameRowOrColumn {
+
+    public int removeStones(int[][] stones) {
+        UnionFindSet set = new UnionFindSet();
+        for (int[] stone : stones) {
+            set.union(set.find(stone[0] + 10001), set.find(stone[1]));
+        }
+        return stones.length - set.rootCount;
+    }
+
+    private class UnionFindSet {
+        private Map<Integer, Integer> roots = new HashMap<>();
+        public int rootCount = 0;
+
+        public int find(int num) {
+            if (!roots.containsKey(num)) {
+                roots.put(num, num);
+                rootCount++;
+            }
+
+            if (num != roots.get(num)) {
+                roots.put(num, find(roots.get(num)));
+            }
+            return roots.get(num);
+        }
+
+        public void union(int a, int b) {
+            int rootA = roots.get(a);
+            int rootB = roots.get(b);
+            if (rootA == rootB) {
+                return;
+            }
+
+            roots.put(rootA, rootB);
+            rootCount--;
+        }
+    }
+}
---
+++ src/com/x5sh1/leetcode/GetMaximumInGeneratedArray.java
@@ -0,0 +1,27 @@
+package com.x5sh1.leetcode
+/*
+https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/581/week-3-january-15th-january-21st/3605/
+ */
+class GetMaximumInGeneratedArray {
+    public int getMaximumGenerated(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 1) {
+            return 1;
+        }
+        int[] array = new int[n + 1];
+        int result = Integer.MIN_VALUE;
+        array[0] = 0;
+        array[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            if (i % 2 == 0) {
+                array[i] = array[i / 2];
+            } else {
+                array[i] = array[(i - 1) / 2] + array[(i - 1) / 2 + 1];
+            }
+            result = result > array[i] ? result : array[i];
+        }
+        return result;
+    }
+}
